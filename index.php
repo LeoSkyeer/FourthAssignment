896368cf537f48e5d7ba199156414927672a22f4
Leopold Skyer <ChernovIU@gmail.ru>
2016-08-25 12:54:53 +1000

    Registration Form V.1.3 (Connect Sucseed)

--- index.php
+++ index.php
@@ -1,32 +1,15 @@
 <?php
 
-require_once ('php/function.php');
+include ('php\function.php');
 
-if ( isset($_POST["add-user"])){
-    if (empty($_POST["user_name"]) || empty($_POST["user_surname"]) || empty($_POST["user_age"])|| empty($_POST["user_message"])) exit ("Данные отсутствуют");
+if (isset($_POST['add_user'])){
+    if(empty($_POST['user_name']) || empty($_POST['user_age']) || empty($_POST['user_message'])) exit ("Данные отсутствуют");
+
 
     $link=connect();
-    addQuery($_POST["user_name"], $_POST["user_surnam"], $_POST["user_age"], $_POST["user_message"]);
-    close($link);
+    addQuery($_POST["user_name"], $_POST["user_age"],$_POST["user_message"],$link);
+    mysqli_close ($link);
 }
-
-//if (isset($_POST["setLogin"])){
-//    $link=connect();
-//    Autorization($_POST["userLogin"], $_POST["userPassword"]);
-//    close($link);
-//    redirect('index.php');
-//}
-//
-//if (isset($_POST["setExit"])){
-//    logout();
-//}
-//
-//if (isset($_GET["action"])&&($_GET["action"]=="accept")&&isAuth()){
-//    $link=connect();
-//    saveAccept($_GET["id"]);
-//    close($link);
-//}
-
 
 ?>
 
@@ -38,42 +21,33 @@ if ( isset($_POST["add-user"])){
     <title>Registration</title>
     <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="/css/bootstrap.css">
-    <link rel="stylesheet" href="/css/bootstrap.min.css.css">
-    <link rel="stylesheet" href="/css/animate.css.css">
-<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
-<!--    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
 </head>
 <body style="background-color:whitesmoke;">
 <div class="col-sm-3 col-sm-offset-4">
     <div class="well" style="margin-top: 10%; background-color:lightsteelblue">
         <h3 style="text-align: center;">Регистрация</h3>
-        <form role="form" id="contactForm" data-toggle="validator" class="shake" action="index.php" method="POST" enctype="multipart/form-data">
+        <form role="form" id="contactForm"  class="shake" action="index.php" method="POST" enctype="multipart/form-data">
 
+            <!-- Имя -->
             <div class="row">
-                <!-- Имя -->
                 <div class="form-group col-sm-12">
                     <label for="name" class="h4">Имя</label>
-                    <input name="user_name" type="text" class="form-control" id="name" placeholder="Введите свое имя" data-error="Забыли как вас зовут?" required>
+                    <input name="user_name" type="text" class="form-control" id="name" placeholder="Введите свое имя" data-error="Забыли как вас зовут?" required pattern="^[А-Яа-яЁё\s]+$">
                     <div class="help-block with-errors"></div>
                 </div>
             </div>
 
-            <div class="row">
-                <!-- Фамилия -->
-                <div class="form-group col-sm-12">
-                    <label for="surname" class="h4">Фамилия</label>
-                    <input name="user_surnam" type="text" class="form-control" id="surname" placeholder="Введите свою фамилию" data-error="Забыли свою фамилию?" required>
-                    <div class="help-block with-errors"></div>
-                </div>
-            </div>
             <!--Возраст-->
             <div class="row">
                 <div class="form-group col-sm-12">
                     <label for="age" class="h4">Возраст</label>
-                    <input name="user_age" type="text" class="form-control" id="age" placeholder="Введите ваш возраст" data-error="Забыли сколько вам лет?" required>
+                    <input name="user_age" type="text" class="form-control" id="age" placeholder="Введите ваш возраст" data-error="Забыли сколько вам лет?" required pattern="^[ 0-9]+$" >
                     <div class="help-block with-errors"></div>
                 </div>
             </div>
+
             <!--Картинка-->
             <div class="row">
                 <div class="form-group col-sm-12">
@@ -81,21 +55,20 @@ if ( isset($_POST["add-user"])){
             <input type="file" class="file" name="fileToUpload" id="fileToUpload">
                 </div>
             </div>
-            <!-- Сам отзыв -->
+
+            <!-- О себе -->
             <div class="row">
             <div class="form-group col-sm-12">
                 <label for="message" class="h4">О себе</label>
                 <textarea name="user_message" id="message" class="form-control" rows="5" placeholder="Напишите немного о себе"  data-error="Необходимо написть хоть что-нибудь" required></textarea>
-                <div class="help-block with-errors"></div>
             </div>
             </div>
+            
             <div class="row">
-                <div class="form-group col-sm-3 col-sm-offset-3">
-                    <input type="submit" class="btn btn-default" value="Зарегистрироваться" name="add-user">
+                <div class="form-group col-sm-3 col-sm-offset-0">
+                    <input type="submit" class="btn btn-default" value="Зарегистрироваться" name="add_user">
                 </div>
             </div>
-            <div id="msgSubmit" class="h3 text-center hidden"></div>
-
             <div class="clearfix"></div>
         </form>
     </div>
--- php/function.php
+++ php/function.php
@@ -1,43 +1,42 @@
 <?php
 $server="localhost";
-$user="root";
-$password="root";
+$login ="root";
+$password="";
+
 
 // Соеденяемся с БД
 function connect(){
-    global $server, $user, $password;
-    $link=mysqli_connect($server, $user, $password);
+    global $server, $login, $password;
+    $link=mysqli_connect($server, $login, $password);
     if (!$link) {
-        die('Error: ' . mysqli_connect_error());
+        die('Error: ' . mysqli_connect_errno());
     }
-    mysqli_set_charset("UTF8", $link);
 
-    $db_selected = mysqli_select_db( "Registration", $link);
+    mysqli_set_charset($link, "UTF8");
+
+    $db_selected = mysqli_select_db($link, "Registration" );
     if (!$db_selected) {
-        exit( print_r(mysqli_connect_error(),true) );
+        exit( print_r(mysqli_connect_errno(),true) );
     }
     return $link;
 }
-//
-//// Вносим данные в БД
-//function addQuery($user_name, $user_surname, $user_age, $user_message){
-//    $query="INSERT INTO Registration
-//				(name, surname, age, text, img)
-//			VALUES
-//				(
-//				'".$user_name."',
-//				'".$user_surname."',
-//				'".$user_age."',
-//				'".$user_message."',
-//				'".'0'."',
-//				'".'0'."',
-//				'".'123'."'
-//      )
-//			";
-//
-//    $result = mysqli_query($query);
-//    if( $result === false ) {
-//        exit( print_r(mysqli_error(),true) );
-//    }
-//    return true;
-//}
+
+
+//ДОбавляем данные
+function addQuery($user_name, $user_age, $user_message, $link){
+    $query="INSERT INTO Registration_Data
+				(name, age, text)
+			VALUES
+				(
+				'".$user_name."',
+				'".$user_age."',
+				'".$user_message."'
+      )
+			";
+
+    $result = mysqli_query($link,$query);
+    if( $result === false ) {
+        exit( print_r(mysqli_connect_errno(),true) );
+    }
+    return true;
+}
